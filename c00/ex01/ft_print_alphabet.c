d8055d8afd9024adc756b5498dc095b7094a97f8
oty <tamejjouothmane@gmail.com>
2024-08-09 03:11:15 +0100

    add c00[ex01]

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+    char x = 'a';
+    while(x <= 'z')
+    {
+        ft_putchar(x);
+        ft_putchar(' ');
+        x++;
+    }   
+}
+int main()
+{
+    ft_print_alphabet();
+}
