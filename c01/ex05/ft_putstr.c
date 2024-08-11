f33ba0d7307d73affa72f49e6e364859ed7ab53e
oty <tamejjouothmane@gmail.com>
2024-08-11 23:02:31 +0100

    add  c01 / ex04

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_putstr(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+    
+}
+
+int main()
+{
+    char *oty = "Hello, I'm Othmane Tamejjou 👋 I am a Software Engineering student";
+    
+    ft_putstr(oty);
+}
