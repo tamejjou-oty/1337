da41e532126c3bd9e45f60be1a350a9493abb7fa
otyhmane <tamejjouothmane@gmail.com>
2024-08-21 21:32:51 +0200

    add ex02

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -19,9 +19,19 @@
     }
     return(0);
 }
+
 int main()
 {
-    char oty[]= "avd.";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "abcdefghijklmnopqrstuvwxyz";
+	char c[] = "contientPasSeuLementDesLettres[";
+	char d[] = "0contientpasSeuLementDeLettres";
+	char e[] = "";
 
-    printf("%d\n", ft_str_is_alpha(oty));
+	printf("%d\n", ft_str_is_alpha(a));
+	printf("%d\n", ft_str_is_alpha(b));
+	printf("%d\n", ft_str_is_alpha(c));
+	printf("%d\n", ft_str_is_alpha(d));
+	printf("%d\n", ft_str_is_alpha(e));
+	return (0);
 }
